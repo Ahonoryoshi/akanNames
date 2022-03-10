3b7a181e706545bde716bd619b5ea9fa93b29deb
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-10 06:34:19 +0300

    replace append method with text to preent overwriting of outputs

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
     $("#blanks form").submit(function(event){
         event.preventDefault();
-    });
-    var dateInput=$("input#date").val();
-    var genderInput=$("input#gender").val();
-    let d = new Date(dateInput);
-    let day = d.getDay();
-    let maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    let weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    if (genderInput==="male"){
+        var dateInput=$("input#date").val();
+        var genderInput=$("input#gender").val();
+        var weekDay=dateInput;
+        let d = new Date(dateInput);
+        let day = d.getDay();
+        let maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+        let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+        let weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        
+        if (genderInput==="male"){
             $(".bday").append(weekDays[day]);
             $(".gen").append(maleNames[day]);
         }
-    else if (genderInput==="female"){
-            $(".bday").append(weekDays[day]);
-            $(".gen").append(femaleNames[day]);
+        else if (genderInput==="female"){
+            $(".bday").text(weekDays[day]);
+            $(".gen").text(femaleNames[day]);
         }
-    else{
+        else{
             alert("Please Enter a Valid Gender")
         }
-    $("#yourInput").show();
+        $("#yourInput").show();
+
+    });
 });
