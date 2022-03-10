f6da674fa8d63d69c0a05f926354a8014081b2ea
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-10 06:23:05 +0300

    add an array to hold days of the week

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,5 +8,6 @@ $(document).ready(function(){
     let day = d.getDay();
     let maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+     let weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 });
