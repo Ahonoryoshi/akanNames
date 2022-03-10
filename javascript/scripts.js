b6f752aaaa0d156d774cf89635b1c72c46819abe
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-10 06:18:06 +0300

    assign variable genderInput to hold gender input

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("#blanks form").submit(function(event){
         event.preventDefault();
     });
+    var dateInput=$("input#date").val();
+    var genderInput=$("input#gender").val();
+
 });
